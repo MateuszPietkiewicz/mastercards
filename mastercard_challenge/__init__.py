4d9fd29368ec97324e6ffae341b9807076c83882
MateuszPietkiewicz <mateuszpietkiewicz1@gmail.com>
2025-05-31 17:27:25 +0200

    analysis eda data start'

--- mastercard_challenge/__init__.py
+++ mastercard_challenge/__init__.py
@@ -1,1 +1,4 @@
 from mastercard_challenge import config  # noqa: F401
+
+from mastercard_challenge.visualisations import numerical_data
+from mastercard_challenge.visualisations import categorical_data
